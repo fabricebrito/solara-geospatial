e839679dbb834df39d5f514d939bd527c31620d8
Fabrice Brito <fabrice.brito@terradue.com>
2023-06-01 14:44:20 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,4 +30,4 @@ destport=$((port + 1))
 
 echo "Using internal port $destport"
 
-jhsingle-native-proxy --destport $destport --authtype none streamlit hello {--}server.port {port} {--}server.headless True {--}server.enableCORS False --port $port
+jhsingle-native-proxy --destport $destport --authtype none solara run "./pages"  {--}host=0.0.0.0 --port $port
